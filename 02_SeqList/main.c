faa2988926cf1109f182271851824c47ae9573c1
lyfff <3070198668@qq.com>
2026-05-10 19:46:14 +0800

    refactor: restructure chapters per course outline, leave impl to student

---
+++ 02_SeqList/main.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#define MaxSize 10
+
+typedef struct
+{
+    int data[MaxSize];
+    int length;
+}Sqlist;
+
+void InitList(Sqlist *L)
+{
+    for (int i=0; i<MaxSize; i++)
+    {
+        L->data[i]=0;
+    }
+    L->length = 0;
+}
+
+int main()
+{
+    Sqlist L;
+    InitList(&L);
+    for (int i=0; i<MaxSize; i++)
+    {
+        printf("%d\n",L.data[i]);
+    }
+    return 0;
+}
