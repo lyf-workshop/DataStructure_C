cc249589c122088b257792217f2dc7e900b4648b
lyfff <3070198668@qq.com>
2026-05-18 20:57:17 +0800

    5.18
    part of LinkedList

--- 03_LinkedList/main.cpp
+++ 03_LinkedList/main.cpp
@@ -29,10 +29,56 @@ Status GetElem_L(LinkList L,int i,ElemType &e) {
     return OK;
 }
 
-Status ListInsert_L(LinkList &L,int i,ElemType &e) {}
+Status ListInsert_L(LinkList &L,int i,ElemType &e) {
+
+    LinkList p = L;
+    int j=0;
+    while (p&&j<i-1) {
+        p = p->next;
+        j++;
+    }
+    if (!p||j>i-1)return ERROR;
+    LNode *pnew = (LNode *)malloc(sizeof(LNode));
+    pnew->data = e;
+    pnew->next = p->next;
+    p->next = pnew;
+}
+
+Status ListDelete_L(LinkList &L,int i,ElemType &e) {
+    LinkList p = L;
+    int j=0;
+    while (p&&j<i-1) {
+        p = p->next;
+        j++;
+    }
+    if (!p->next||j>i-1)return ERROR;
+    LinkList q = p->next;
+    p->next = q->next;
+    e = q->data;
+    free(q);
+    return OK;
+}
+
+void CreatList_L(LinkList &L,int n) {
+    L = (LinkList)malloc(sizeof(LNode));
+    L->next = NULL;
+    for (int i =n;i>0;i--) {
+        LinkList p = (LinkList)malloc(sizeof(LNode));
+        scanf("%d",&p->data);
+        p->next = L->next;
+        L->next = p;
+    }
+}
 
 int main(void) {
-    LinkList L = (LinkList)malloc(sizeof(struct ListNode));
+    printf("hello world\n");
+    LinkList L;
+    CreatList_L(L,10);
+    int i ,j=0;
+    for (i=1;i<=10;i++) {
+        GetElem_L(L,i,j);
+        printf("%d\n",j);
+    }
 
     return 0;
 }
