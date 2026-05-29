7a76f4a8cc9f51ffcd9486c41df29a0ff0642d28
lyfff <3070198668@qq.com>
2026-05-29 20:41:17 +0800

    refactor(linked-list): Modularize linked list implementations (模块化链表实现)
    本次提交是对整个链表模块的重构，主要是为了遵循更好的软件工程实践，提升代码的可维护性和模块化程度。
    
    在之前的版本中，所有不同类型的链表操作、定义和初始化功能冗余地集中在 `main.cpp` 文件中，导致该文件结构臃肿，难以阅读和维护。本次重构解决了这一问题，将不同链表类型的核心逻辑（如单链表、双向链表、静态链表的操作函数）分别提取到独立的 `.h` 和 `.cpp` 文件中。
    
    这套改进使代码结构更加清晰，每个功能模块都拥有了自己的职责范围。只需要在 `CMakeLists.txt` 中将所有模块引入到编译目录，同时极大地简化了 `main.cpp` 的内容，使其专注于业务流程的调用，而不是功能的实现细节。这样做的核心目标是实现代码的职责分离（Separation of Concerns）。

--- 03_LinkedList/main.cpp
+++ 03_LinkedList/main.cpp
@@ -1,176 +1,23 @@
-﻿#define TRUE          1
-#define FALSE         0
-#define OK            1
-#define ERROR         0
-#define INFEASIBLE   -1
-#define OVERFLOW     -2
-#define LIST_INIT_SIZE 100
-#define LIST_INCREMENT 10
-#define ElemType int
-#include <stdlib.h>
-typedef int Status;
+﻿#include "singly_linked_list.h"
 
 #include <stdio.h>
-
-typedef struct ListNode {
-    ElemType data;
-    struct ListNode *next;
-}LNode,*LinkList;
-
-typedef struct DuLNode {
-    ElemType data;
-    struct DuLNode *next;
-    struct DuLNode *prior;
-}DuLNode,*DuLinkList;
-
-Status ListInsert_DuL(DuLinkList &L) {
-    if (!())
-        return ERROR;
-
-}
-
-typedef struct {
-    ElemType data;
-    int cur;
-}component,SLinkList[LIST_INIT_SIZE];
-
-int LocateElem_SL(SLinkList &S,ElemType e) {
-    int i=S[0].cur;
-    while (i&&S[i].data!=e) {
-        i=S[i].cur;
-    }
-    return i;
-}
-
-void InitList_SL(SLinkList &space) {
-    for (int i =0;i<LIST_INIT_SIZE-1;i++) {
-        space[i].cur = i+1;
-    }
-    space[LIST_INIT_SIZE-1].cur = 0;
-}
-
-int Malloc_SL(SLinkList &space) {
-    int i = space[0].cur;
-    if (space[0].cur) {
-        space[0].cur = space[i].cur;
-    }
-}
-
-void Free_SL(SLinkList &space,int k) {
-    space[k].cur = space[0].cur;
-    space[0].cur = k;
-}
-
-void difference(SLinkList &space,int &S) {
-    InitList_SL(space);
-    S = Malloc_SL(space);
-    int r = S;
-    scanf(m,n);
-}
-
-Status GetElem_L(LinkList L,int i,ElemType &e) {
-    LinkList p = L->next;
-    int j=1;
-    while (p&&j<i) {
-        p = p->next;
-        j++;
-    }
-    if (!p||j>i)return ERROR;
-    e = p->data;
-    return OK;
-}
-
-Status ListPriorInsert_L(LinkList &P,ElemType &e) {
-    if (!P) return ERROR;
-    LinkList p = (LinkList)malloc(sizeof(LNode));
-    if (!p) return ERROR;
-    p ->next = P->next;
-    P->next = p;
-    p->data = P->data;
-    P->data = e;
-    return OK;
-}
-
-Status ListInsert_L(LinkList &L,int i,ElemType &e) {
-
-    LinkList p = L;
-    int j=0;
-    while (p&&j<i-1) {
-        p = p->next;
-        j++;
-    }
-    if (!p||j>i-1)return ERROR;
-    LNode *pnew = (LNode *)malloc(sizeof(LNode));
-    pnew->data = e;
-    pnew->next = p->next;
-    p->next = pnew;
-    return OK;
-}
-
-Status ListDelete_L(LinkList &L,int i,ElemType &e) {
-    LinkList p = L;
-    int j=0;
-    while (p&&j<i-1) {
-        p = p->next;
-        j++;
-    }
-    if (!p->next||j>i-1)return ERROR;
-    LinkList q = p->next;
-    p->next = q->next;
-    e = q->data;
-    free(q);
-    return OK;
-}
-
-void CreatList_L(LinkList &L,int n) {
-    L = (LinkList)malloc(sizeof(LNode));
-    L->next = NULL;
-    for (int i =n;i>0;i--) {
-        LinkList p = (LinkList)malloc(sizeof(LNode));
-        scanf("%d",&p->data);
-        p->next = L->next;
-        L->next = p;
-    }
-}
-
-void MergeList_L(LinkList &La,LinkList &Lb,LinkList &Lc) {
-    LinkList pa = La->next;
-    LinkList pb = Lb->next;
-    LinkList pc;
-    Lc=pc=La;
-    while (pa&&pb) {
-        if (pa->data <= pb->data) {
-            pc->next = pa;
-            pc = pa;
-            pa = pa->next;
-        }
-        else {
-            pc->next = pb;
-            pc = pb;
-            pb = pb->next;
-        }
-        pc->next =pa?pa:pb;
-        free(Lb);
-    }
-}
 
 int main(void) {
     printf("hello world\n");
     LinkList La;
     CreatList_L(La,10);
-    int i ,j=0;
-    for (i=1;i<=10;i++) {
-        GetElem_L(La,i,j);
-        printf("%d\t",j);
+    int i, j = 0;
+    for (i = 1; i <= 10; i++) {
+        GetElem_L(La, i, j);
+        printf("%d\t", j);
     }
     LinkList Lb;
     CreatList_L(Lb,10);
     LinkList Lc;
     MergeList_L(La,Lb,Lc);
-    for (i=1;i<=10;i++) {
-        GetElem_L(Lc,i,j);
-        printf("%d\t",j);
+    for (i = 1; i <= 10; i++) {
+        GetElem_L(Lc, i, j);
+        printf("%d\t", j);
     }
     return 0;
 }
-
